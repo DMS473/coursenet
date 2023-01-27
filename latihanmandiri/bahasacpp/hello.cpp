0d75e5c0bb6b7c197e0529d7f881b5f733a780c0
DMS473 <daffamuis0407@gmail.com>
2023-01-27 13:47:33 +0700

    inisiasi repo awal

---
+++ latihanmandiri/bahasacpp/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+    std::cout << "Hello world!";
+    std::cout << "Hello world!";
+    std::cout << "Hello world!";
+
+    return 0;
+}
